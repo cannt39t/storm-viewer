0df56adaf8eeb76253eccb08c8b0e7a4bd522987
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-04-13 20:34:25 +0300

    Initial commit

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Илья Казначеев on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+
+
+}
+
